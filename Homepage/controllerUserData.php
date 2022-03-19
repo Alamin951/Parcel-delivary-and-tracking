d1d4075b8e64250576c0e80a6f8fd7fa9def15da
prodhan482 <96787662+prodhan482@users.noreply.github.com>
2022-03-19 21:48:32 +0600

    updated

--- Homepage/controllerUserData.php
+++ Homepage/controllerUserData.php
@@ -21,7 +21,7 @@ if (isset($_POST['signup'])) {
     if ($password !== $cpassword) {
         $errors['password'] = "Confirm password not matched!";
     }
-    $email_check = "SELECT * FROM usertable WHERE email = '$email'";
+    $email_check = "SELECT * FROM userinfo WHERE email = '$email'";
     $res = mysqli_query($con, $email_check);
     if (mysqli_num_rows($res) > 0) {
         $errors['email'] = "Email that you have entered is already exist!";
@@ -30,7 +30,7 @@ if (isset($_POST['signup'])) {
         $encpass = password_hash($password, PASSWORD_BCRYPT);
         $code = rand(999999, 111111);
         $status = "not_verified";
-        $insert_data = "INSERT INTO usertable (name, email, phone, gender, address,  password, code, status)
+        $insert_data = "INSERT INTO userinfo (name, email, phone, gender, address,  password, code, status)
                         values('$name', '$email', '$phone','$gender','$address', '$encpass', '$code', '$status')";
         $data_check = mysqli_query($con, $insert_data);
         if ($data_check) {
@@ -58,7 +58,7 @@ if (isset($_POST['signup'])) {
 if (isset($_POST['check'])) {
     $_SESSION['info'] = "";
     $otp_code = mysqli_real_escape_string($con, $_POST['otp']);
-    $check_code = "SELECT * FROM usertable WHERE code = $otp_code";
+    $check_code = "SELECT * FROM userinfo WHERE code = $otp_code";
     $code_res = mysqli_query($con, $check_code);
     if (mysqli_num_rows($code_res) > 0) {
         $fetch_data = mysqli_fetch_assoc($code_res);
@@ -66,7 +66,7 @@ if (isset($_POST['check'])) {
         $email = $fetch_data['email'];
         $code = 0;
         $status = 'verified';
-        $update_otp = "UPDATE usertable SET code = $code, status = '$status' WHERE code = $fetch_code";
+        $update_otp = "UPDATE userinfo SET code = $code, status = '$status' WHERE code = $fetch_code";
         $update_res = mysqli_query($con, $update_otp);
         if ($update_res) {
             $_SESSION['name'] = $name;
@@ -86,7 +86,7 @@ if (isset($_POST['check'])) {
 if (isset($_POST['login'])) {
     $email = mysqli_real_escape_string($con, $_POST['email']);
     $password = mysqli_real_escape_string($con, $_POST['password']);
-    $check_email = "SELECT * FROM usertable WHERE email = '$email'";
+    $check_email = "SELECT * FROM userinfo WHERE email = '$email'";
     $res = mysqli_query($con, $check_email);
     if (mysqli_num_rows($res) > 0) {
         $fetch = mysqli_fetch_assoc($res);
@@ -115,11 +115,11 @@ if (isset($_POST['login'])) {
 
 if (isset($_POST['check-email'])) {
     $email = mysqli_real_escape_string($con, $_POST['email']);
-    $check_email = "SELECT * FROM usertable WHERE email='$email'";
+    $check_email = "SELECT * FROM userinfo WHERE email='$email'";
     $run_sql = mysqli_query($con, $check_email);
     if (mysqli_num_rows($run_sql) > 0) {
         $code = rand(999999, 111111);
-        $insert_code = "UPDATE usertable SET code = $code WHERE email = '$email'";
+        $insert_code = "UPDATE userinfo SET code = $code WHERE email = '$email'";
         $run_query =  mysqli_query($con, $insert_code);
         if ($run_query) {
             $subject = "Password Reset Code";
@@ -147,7 +147,7 @@ if (isset($_POST['check-email'])) {
 if (isset($_POST['check-reset-otp'])) {
     $_SESSION['info'] = "";
     $otp_code = mysqli_real_escape_string($con, $_POST['otp']);
-    $check_code = "SELECT * FROM usertable WHERE code = $otp_code";
+    $check_code = "SELECT * FROM userinfo WHERE code = $otp_code";
     $code_res = mysqli_query($con, $check_code);
     if (mysqli_num_rows($code_res) > 0) {
         $fetch_data = mysqli_fetch_assoc($code_res);
@@ -173,7 +173,7 @@ if (isset($_POST['change-password'])) {
         $code = 0;
         $email = $_SESSION['email'];
         $encpass = password_hash($password, PASSWORD_BCRYPT);
-        $update_pass = "UPDATE usertable SET code = $code, password = '$encpass' WHERE email = '$email'";
+        $update_pass = "UPDATE userinfo SET code = $code, password = '$encpass' WHERE email = '$email'";
         $run_query = mysqli_query($con, $update_pass);
         if ($run_query) {
             $info = "Successfully changed your password. Now you can login with your new password.";
